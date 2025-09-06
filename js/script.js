db09b92cda748d0330ef8a1f7129af69f329ab09
kiana.kad <kianakadivar05@gmail.com>
2025-09-06 20:13:36 +0330

    feat: add horizontal scroll animation with GSAP ScrollTrigger
    - Implemented a timeline pinned to .main-wrapper.
    - Synced horizontal x movement with scroll using ScrollTrigger.
    - Applied minor style adjustment for layout consistency.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 "use strict"
+
+const container=document.querySelector(".main-wrapper")
+
+let tl=gsap.timeline({
+    scrollTrigger:{
+        trigger:container,
+        scrub:1,
+        pin:true,
+        start:"top top",
+        end:"+=1000"
+    }
+})
+.to(container,{
+    x:()=>
+        -(container.scrollWidth - document.documentElement.clientWidth) + "px",
+    ease:"none",
+    duration:1,
+})
